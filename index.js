24f751a10ad999650d4c6dd12c7fca288cd870a2
GAMch1k <gammail404@gmail.com>
2025-08-23 01:12:00 +0200

    added cors as *

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = 1873
 
 app.use(express.static("."))
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
